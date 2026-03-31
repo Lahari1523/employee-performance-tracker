2d204bfc62ea93de323e32f99e951159701841ad
Lahari1523 <besthalahari1502@gmail.com>
2026-03-31 12:00:49 +0530

    Update database_setup.py

--- database_setup.py
+++ database_setup.py
@@ -1,1 +1,18 @@
+import sqlite3
 
+conn = sqlite3.connect('employee.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS employees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    department TEXT,
+    month TEXT,
+    performance_score INTEGER
+)
+''')
+
+conn.commit()
+conn.close()
+print("Database and table created successfully.")
